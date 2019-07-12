7b5886c874c515b88efe5406a193dffbb2d4f15c
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-07-12 11:09:48 -0500

    finished cityreader

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,14 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
+class City:
+  def __init__(self,name,lat,lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
+
+  def __str__(self):
+    return f"Name: {self.name} Lat: {self.lat} Lon: {self.lon}"
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -14,12 +22,30 @@
 #
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
+import csv
 cities = []
 
 def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
+
+    with open('cities.csv') as csv_file:
+      csv_reader = csv.reader(csv_file, delimiter=',')
+      line_count = 0
+      lat_index = None
+      lon_index = None
+      name_index = None
+      for row in csv_reader:
+          if line_count == 0:
+              lat_index = row.index("lat")
+              lon_index = row.index("lng")
+              name_index = row.index("city")
+              line_count += 1
+          else:
+              cities.append(City(row[name_index],row[lat_index],row[lon_index]))
+              line_count += 1
+    print(f'Processed {line_count} lines.')
     
     return cities
 
